4919f1c441a723de67344a1771198f510b480d4d
raipier8818 <raipier8818@hanyang.ac.kr>
2021-09-25 15:58:16 +0900

    update

--- serverless React SPA/forif-practice/.eslintrc.js
+++ serverless React SPA/forif-practice/.eslintrc.js
@@ -1,24 +1,29 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node":true,
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    'plugin:import/error',
-    'plugin:import/warning',
-    'airbnb',
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 12
-  },
-  "plugins": [
-    // "@typescript-eslint"
-  ],
-  "rules": {
-    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
-  }
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        // "airbnb",
+        // 'plugin:import/recommended'
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+        "quotes": "double",
+    }
 };
